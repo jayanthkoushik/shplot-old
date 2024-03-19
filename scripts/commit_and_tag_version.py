8f7e34089b7792e41ea674c081abc060c82a4692
Jayanth Koushik <mail@jkoushik.me>
2024-03-18 21:14:20 -0400

    feat: first

--- scripts/commit_and_tag_version.py
+++ scripts/commit_and_tag_version.py
@@ -4,32 +4,31 @@ import subprocess
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
+parser.add_argument("-f", "--first-release", type=str, choices=["true", "false"])
 parser.add_argument("-r", "--release-version", nargs="?", type=str)
 parser.add_argument("-p", "--pre-release", type=str, choices=["true", "false"])
 parser.add_argument("-t", "--pre-release-tag", nargs="?", type=str)
 parser.add_argument("--dry-run", action="store_true")
 args = parser.parse_args()
 
-cmd_version_args = []
+cmd_args = []
+
+if args.first_release == "true":
+    cmd_args.append("--skip.changelog")
+
 if args.release_version:
-    cmd_version_args = ["-r", args.release_version]
+    cmd_args.extend(["-r", args.release_version])
+elif args.first_release == "true":
+    cmd_args.extend(["-r", "1.0.0"])
 
-pre_release_args = []
 if args.pre_release == "true":
-    pre_release_args = ["-p"]
+    cmd_args.append("-p")
     if args.pre_release_tag:
-        pre_release_args.append(args.pre_release_tag)
+        cmd_args.append(args.pre_release_tag)
 
-dry_run_args = []
 if args.dry_run:
-    dry_run_args = ["--dry-run"]
+    cmd_args.append("--dry-run")
 
-cmd = [
-    "npx",
-    "commit-and-tag-version",
-    *cmd_version_args,
-    *pre_release_args,
-    *dry_run_args,
-]
+cmd = ["npx", "commit-and-tag-version", *cmd_args]
 print(f"+ {' '.join(cmd)}")
 subprocess.run(cmd, check=True)
